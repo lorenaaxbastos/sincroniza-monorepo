dfae3dbf17127143afa7b68844325dfbd1ad8986
Lorena Bastos <lorenaax.bastos@hotmail.com>
2026-06-02 18:05:28 -0300

    chore(infra): initialize and configure changesets

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  viteFinal: (config) => {
+    config.plugins = config.plugins ?? [];
+    config.plugins.push(tsconfigPaths());
+
+    config.esbuild = {
+      ...config.esbuild,
+      jsx: 'automatic',
+    };
+
+    return config;
+  },
+};
+
+export default config;
