32d9a57d708662027bcdd841ab44268bbee27301
Lorena Bastos <lorenaax.bastos@hotmail.com>
2026-06-16 15:39:48 -0300

    feat: use prolata as test for components

---
+++ apps/lms/src/app.config.ts
@@ -0,0 +1,12 @@
+export const appConfig = {
+  id: '000',
+  titleSuffix: ' | AVA - Sincroniza Educação',
+  defaultDescription:
+    'Ambiente de Aprendizagem Virtual (AVA) da Sincroniza Educação',
+  googleAnalyticsId: {
+    212: 'G-X283Y8WV2H',
+    223: 'G-0HPGKVWMKE',
+    248: 'G-99G93VBBGS',
+  },
+  handTalkToken: '263b54729fcf716093ffb3cd83776e22',
+};
